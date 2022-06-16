cbc2295ea204c2c6d9901f8646ca147f2ab4a25a
MariaDuda <mariabevenuto062@gmail.com>
2022-06-16 17:48:41 -0300

    find failed

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
 import express , {json}  from "express";
 import cors from "cors";
 
-
-
 const app = express()
 app.use(json())
 app.use(cors())
 
-let users = []
-let tweets = [];
+const users = []
+const tweets = [];
 
-app.post("/sign-up" , (req,res)=>{
- const {username , avatar} = req.body
-  users.push({username,avatar})
-   // aqui n ta indo , ta dando erro no get
-  if (!username || !avatar) {
-    res.sendStatus(400)
+
+app.post("/sign-up", (req, res) => {
+  const {username, avatar} = req.body;
+  if(!username || !avatar) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
   }
-  console.log(users)
-  res.sendStatus(201)
- 
-})
+    users.push({username, avatar});
+
+    res.status(200).send("OK!");
+    
+   
+});
+
 
 app.get("/tweets", (req, res) => {
   const ultimos = tweets.slice(-10);
   res.send(ultimos);
 });
 
-app.post("/tweets" , (req, res)=>{
-  const {username , tweet} = req.body
-  tweets.push({username,tweet})
+app.get("/tweets/:user", (req, res) => {
+  const { user } = req.params;
+
+  const usertweets = tweets.filter(tweet => {
+      if (tweet.username === user) return tweet;
+  });
+
+  res.send(usertweets)
+});
+
+
+app.post("/tweets", (req, res) => {
+  const username = req.headers.user;
+  const { tweet } = req.body;
+  const data = { username, tweet };
+   console.log(data.username)
   
   if (!username || !tweet) {
-    res.sendStatus(400)
+    res.sendStatus(400);
   }
-  console.log(tweets)
-  res.sendStatus(201)
 
- })
- 
- 
- 
+  const usuario = users.find((user) => user.username === data.username);
+  console.log(usuario)
+  
+  
+  res.sendStatus(201);
+});
+
 
 app.listen(5000 , ()=>{
 console.log("i`m alive")
