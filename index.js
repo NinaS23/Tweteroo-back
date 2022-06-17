ab62ba3767e5ff85b94869bca94445b6b2f42d2f
MariaDuda <mariabevenuto062@gmail.com>
2022-06-17 12:38:38 -0300

    tab to organize

--- index.js
+++ index.js
@@ -8,53 +8,46 @@ app.use(cors())
 const users = []
 const tweets = [];
 
-
 app.post("/sign-up", (req, res) => {
-  const {username, avatar} = req.body;
-  if(!username || !avatar) {
-    res.status(400).send("Todos os campos são obrigatórios!");
-    return;
+   const {username, avatar} = req.body;
+   if(!username || !avatar) {
+      res.status(400).send("Todos os campos são obrigatórios!");
+      return;
   }
-    users.push({username, avatar});
-    res.status(201).send("created");
-    
-   
+      users.push({username, avatar});
+      res.status(201).send("created");
 });
 
-
 app.get("/tweets", (req, res) => {
-  const ultimos = tweets.slice(-10);
-  res.send(ultimos);
+    const ultimos = tweets.slice(-10);
+    res.send(ultimos);
 });
 
 app.get("/tweets/:user", (req, res) => {
-  const { user } = req.params;
-
-  const usertweets = tweets.filter(tweet => {
-      if (tweet.username === user) return tweet;
+    const { user } = req.params;
+    const usertweets = tweets.filter(tweet => {
+        if (tweet.username === user) return tweet;
   });
-
-  res.send(usertweets)
+    res.send(usertweets)
 });
 
-
 app.post("/tweets", (req, res) => {
-  const username = req.headers.user;
-  const { tweet } = req.body;
+    const username = req.headers.user;
+    const { tweet } = req.body;
   
-  if (!username || !tweet) {
-    res.status(400).send("Todos os campos são obrigatórios!");
-    return;
+    if (!username || !tweet) {
+      res.status(400).send("Todos os campos são obrigatórios!");
+      return;
   }
-  let avatar = users.find(user => user.username === username)
-  avatar = avatar || {avatar: ''}
+   let avatar = users.find(user => user.username === username)
+       avatar = avatar || {avatar: ''}
   tweets.push({
       username: username,
       avatar: avatar.avatar,
       tweet: tweet
   })
-  console.log(tweets)
-  res.status(201).send("CREATED")
+    console.log(tweets)
+    res.status(201).send("CREATED")
 });
 
 
